2995c48e4fc7d9b9508b00da74464a1ce1da09fb
Svet21 <chuyak.svetlana@gmail.com>
2021-07-22 12:10:56 +0500

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket_price = 10_000;
+        int mile_size = 20;
+        int mile_amount = ticket_price / mile_size;
+        System.out.println(mile_amount);
+
+    }
+}
+
